d37144fd25cb71fe040c906d1c03c9335b087645
Luis <siquike@gmail.com>
2016-08-30 19:35:42 -0400

    Q1.1 seems to be done. Check with TAs to see if format of filter_Response is appropriate

--- hw1.m
+++ hw1.m
@@ -3,7 +3,7 @@
 % The higher resolution the filter, the more smoothness is applied
 
 % Gaussian Filters: These filters provide the light vs color properties as
-% features
+% featurespon
 % Log Scale Filters: These filters provide edges vs smooth surfaces
 % dx Scales: These filters provide vertical edges
 % dy Scales: These filters provide horizontal edges
@@ -12,6 +12,7 @@
 filterBank = createFilterBank(); 
 % I1= imread('sun_aerinlrdodkqnypz.jpg');
 I1= gpuArray(imread('sun_aerinlrdodkqnypz.jpg'));
-%%
 
-[filterResponses] = extractFilterResponses(I1, filterBank);
+filter_Response = extractFilterResponses(I1, filterBank);
+
+image(filter_Response)
