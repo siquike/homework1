c51428246385a7f53655aa10d92ba5e778c04def
Luis <siquike@gmail.com>
2016-09-15 19:54:30 -0400

    Question 1.1 is done

--- hw1.m
+++ hw1.m
@@ -11,8 +11,41 @@
 
 filterBank = createFilterBank(); 
 % I1= imread('sun_aerinlrdodkqnypz.jpg');
-I1= gpuArray(imread('sun_aerinlrdodkqnypz.jpg'));
+I1= gpuArray(imread('sun_aadolwejqiytvyne.jpg'));
 
 filter_Response = extractFilterResponses(I1, filterBank);
+filter_Response = gather(filter_Response);
+npx = size(filter_Response,1);
+npy = size(filter_Response,2);
+npz = 3*ones(1,20);
+% filter_Response = reshape(filter_Response,187500,60);
 
-image(filter_Response)
+for i = 1:length(filterBank)
+   filter_Response_m(:,:,:,i) = filter_Response(:,:,3*i-2:3*i);
+end
+
+montage(filter_Response_m,'Size',[4 5])
+
+%% Q1.2
+
+% montage(cell_filter_Response,'Parent',cell_filter_Response)
+% parfor i= 1:length(filterBank)
+%     cellnew{i,1} = filter_Response(:,:,3*i-2:3*i);
+% end
+% parfor i:length(filterBank)
+%     filres = gather(filter_Response());
+% end
+% image(filter_Response(:,:,7:9))
+
+% K = 3;
+% alpha = 20;
+% xidx = randperm(size(filter_Response,1),alpha);
+% yidx = randperm(size(filter_Response,2),alpha);
+% sample = filter_Response(xidx,yidx);
+
+
+% filter_Response = reshape(filter_Response,187500,60);
+% [idx, dictionary] = kmeans(filter_Response, K, 'EmptyAction','drop');
+
+% filter_Response = reshape(filter_Response,187500,60);
+% [idx, dictionary] = kmeans(filter_Response, K, 'EmptyAction','drop');
